ec33e0eaf32631eb06f5e01d7d6917fa2ccc312f
ImDen-Dev <neckromant90@gmail.com>
2025-02-21 14:41:58 +0200

    minor fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,7 +10,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
-import { firebaseConfig } from './assets/environments';
+import { firebaseConfig } from './assets/environments/environments';
 
 export const appConfig: ApplicationConfig = {
   providers: [
